03c143ccff8272bb39bcc086c19aa999967a38c6
Kyon_dreamer <dream7652210@gmail.com>
2020-02-28 23:08:17 +0800

    2/28_Startmenu_01 add: background start_button setting_button end_button

---
+++ Assets/Scripts/Start/S_Button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class S_Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void p_start()
+    {
+        SceneManager.LoadScene("Demo");
+    }
+    public void p_end()
+    {
+        Application.Quit();
+    }
+}
