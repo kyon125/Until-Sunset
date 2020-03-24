26ad19c1888450b8825c18c88a5848fac7495b4f
Kyon_dreamer <dream7652210@gmail.com>
2020-03-24 16:32:39 +0800

    在demo 場景中，新增彈藥選擇ui 按e鍵開啟功能，左右鍵選擇 再按一次e鍵關閉

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController2D : MonoBehaviour
+{
+
+    private float speed = 1.0f;
+    private Rigidbody2D Rigidbody;
+    private Collider2D Collider; 
+    private float speed_X=5.0f;
+   
+    public bool isGrounded;
+    public LayerMask groundLaters;
+
+    public bool isHided;
+    public LayerMask hideLayers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+        Collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // isground
+       isGrounded= Physics2D.OverlapArea(new Vector2(transform.position.x -1.0f, transform.position.y -1.0f), new Vector2(transform.position.x + 1.0f, transform.position.y - 1.0f), groundLaters);
+
+        //ishide
+        isHided= Physics2D.OverlapArea(new Vector2(transform.position.x -1.0f, transform.position.y), new Vector2(transform.position.x + 1.0f, transform.position.y),hideLayers);
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rigidbody.AddForce(new Vector2(100 * speed, 0), ForceMode2D.Impulse);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Rigidbody.AddForce(new Vector2(-100 * speed, 0), ForceMode2D.Impulse);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true) 
+        {
+            Rigidbody.AddForce(new Vector2(0, 30), ForceMode2D.Impulse);
+        }
+
+        // Hide
+        if (Input.GetKeyDown(KeyCode.S) && isHided == true)
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && isHided == true)
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+
+        // 速度限制
+        if (Rigidbody.velocity.x > speed_X)
+        {
+            Rigidbody.velocity = new Vector2(speed_X, Rigidbody.velocity.y);
+        }
+        else if (Rigidbody.velocity.x < -speed_X)
+        {
+            Rigidbody.velocity = new Vector2(-speed_X, Rigidbody.velocity.y);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Common/UIsetting.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIsetting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject [] depot= new GameObject[3];
+    GameObject mid;
+    public GameObject p0, p1, p2, p3;
+    private int midnum;
+    private bool depotopen = false;
+    void Start()
+    {
+        initial();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        B_depotopen();
+        U_select();
+    }
+    void initial()
+    {
+        depot[0] = p1;
+        depot[1] = p2;
+        depot[2] = p3;
+
+        mid = Instantiate(depot[0] ,new Vector3(0,0,0), Quaternion.identity, p0.transform);
+        RectTransform rt = mid.GetComponent<RectTransform>();
+        rt.anchoredPosition = new Vector3(0, 0, 0);
+
+        midnum = 0;
+    }
+    void U_select()
+    {
+        if (depotopen == true)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow) && midnum < 2)
+            {
+                Destroy(mid);
+                midnum++;
+                mid = Instantiate(depot[midnum], new Vector3(0, 0, 0), Quaternion.identity, p0.transform);
+                RectTransform rt = mid.GetComponent<RectTransform>();
+                rt.anchoredPosition = new Vector3(0, 0, 0);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && midnum > 0)
+            {
+                Destroy(mid);
+                midnum--;
+                mid = Instantiate(depot[midnum], new Vector3(0, 0, 0), Quaternion.identity, p0.transform);
+                RectTransform rt = mid.GetComponent<RectTransform>();
+                rt.anchoredPosition = new Vector3(0, 0, 0);
+            }
+        }  
+    }
+
+    void B_depotopen()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && depotopen == false)
+        {
+            depotopen = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.E) && depotopen == true)
+        {
+            depotopen = false;
+        }
+    }
+}
