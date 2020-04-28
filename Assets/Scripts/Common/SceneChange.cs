e6779d0171f45b1832100be9f66434ca6419a89c
Kyon_dreamer <dream7652210@gmail.com>
2020-04-28 23:54:33 +0800

    4/28

---
+++ Assets/Scripts/Common/SceneChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string loadname;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (loadname != "")
+            loadscene();
+    }
+    void loadscene()
+    {
+        SceneManager.LoadScene(loadname);
+    }
+}
